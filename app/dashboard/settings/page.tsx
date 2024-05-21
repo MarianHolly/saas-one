fe4076d67e6450142460fda66ef3659a798f6a29
Stupel <hellomariandev@gmail.com>
2024-05-21 20:50:18 +0200

    ,

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -8,7 +8,6 @@ import {
 } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
 
 import prisma from "@/lib/db";
 import { revalidatePath, unstable_noStore as noStore } from "next/cache";
